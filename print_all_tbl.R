07511ee75b28694a0606dd36d6833f5f6eee9e98
vbaudrot <virgile-baudrot@users.noreply.github.com>
2017-05-22 14:11:21 +0200

    Update print_all_tbl.R

--- print_all_tbl.R
+++ print_all_tbl.R
@@ -1,4 +1,8 @@
-print_all_tbl = function(df){
-  df2 = df %>% tbl_df %>% print(n = nrow(.))
+print_all_tbl = function(df, nbr_row=FALSE){
+  if(nbr_row != FALSE){
+    df2 = df %>% tbl_df %>% print(n = nbr_row)
+  } else{
+    df2 = df %>% tbl_df %>% print(n = nrow(.))
+  }  
   return(df2)
 }
